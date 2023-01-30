9540c29a7a4c99a1675114b8aa7dbda9b8ecc4da
Ben Terova <ben@evilsu.com>
2023-01-29 21:53:49 -0500

    Header, navbar and bulma

---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    
+</script>
+
+<section class="hero">
+    <div class="hero-body">
+      <p class="title">
+        Ben Terova
+      </p>
+      <p class="subtitle">
+        Software Engineer | Full Stack Developer
+      </p>
+    </div>
+  </section>
+
+<style>
+    .subtitle{
+        font-style: italic;
+    }
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  // Toggle for hamburger menu
+  const toggleMenu = () => {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("is-active");
+  };
+</script>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img
+        src="https://bulma.io/images/bulma-logo.png"
+        width="112"
+        height="28"
+      />
+    </a>
+
+    <a
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false"
+      on:click={toggleMenu}
+      href="#"
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </a>
+  </div>
+
+  <div id="navbar" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item"> Work </a>
+
+      <a class="navbar-item"> Side-Projects </a>
+
+      <a class="navbar-item"> Contact </a>
+    </div>
+  </div>
+</nav>
